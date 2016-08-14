c55d7437f24c0a7e23bfd5a4d1138b09bb4de940
Yuya TAMANO <everfree.main@gmail.com>
2016-08-14 20:43:08 +0900

    Add Validation for instruction files.

--- lib/dustcart/cli.rb
+++ lib/dustcart/cli.rb
@@ -1,13 +1,46 @@
 # coding: utf-8
 
 require 'thor'
+require 'unindent'
 
 module Dustcart
   # command facade
   class CLI < Thor
+    desc 'exec', 'execute backup.'
+    option :instruction_file, type: :string, required: false, aliases: '-f'
+    def exec
+      instruction_file = (options[:instruction_file] || "#{Dir.home}/.dustcart/default.rb")
+
+      begin
+        check_file(instruction_file)
+      rescue => error
+        puts "ERROR: #{error.message}"
+        exit 1
+      end
+
+
+      # TODO: call DSL
+      # puts "TARGET: #{instruction_file}"
+    end
+
     desc 'version', 'show version information.'
     def version
       puts Dustcart::VERSION
+    end
+
+    private
+
+    def check_file(file)
+      raise <<-EOS.unindent unless File.exist?(file)
+        The instruction file (#{file}) doesn't exists.
+      EOS
+
+      mode = format('%o', File.stat(file).mode)[-3, 3]
+
+      raise <<-EOS.unindent unless mode == '600'
+        The instruction file (#{file}) has too open permission.
+        Make sure its permission is 600.
+      EOS
     end
   end
 end
---
+++ spec/dustcart/cli_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Dustcart::CLI do
+  xdescribe '#exec'
+  xdescribe '#version'
+  xdescribe '#check_file!'
+end
