8781e1be7236cc8a793f0ffc75a18157c79a03b5
Yuya TAMANO <everfree.main@gmail.com>
2016-08-14 17:17:15 +0900

    Install simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dustcart'
+
+require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/spec/'
+  track_files '{exe,lib}/**/*.rb'
+end
