cc7a8c1ae27be379b004162d14a5c65965966888
Yuya TAMANO <everfree.main@gmail.com>
2016-08-16 02:13:23 +0900

    Refactore Group

--- lib/dustcart.rb
+++ lib/dustcart.rb
@@ -2,8 +2,6 @@ require 'dustcart/version'
 require 'dustcart/cli'
 require 'dustcart/dsl'
 require 'dustcart/group'
-require 'dustcart/input'
-require 'dustcart/output'
 
 # Main module
 module Dustcart
