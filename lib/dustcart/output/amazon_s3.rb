ad06531cbe726dca19086615f7ccae31f6df0e87
Yuya TAMANO <everfree.main@gmail.com>
2016-08-16 21:15:29 +0900

    Implement AmazonS3#precheck

--- lib/dustcart/output/amazon_s3.rb
+++ lib/dustcart/output/amazon_s3.rb
@@ -10,6 +10,17 @@ module Dustcart
 
         def precheck
           super
+
+          raise <<-EOS.unindent unless mode == :all
+            mode (#{mode}) unknown.
+            :all is the only available mode now.
+          EOS
+
+          [:access_key_id, :secret_access_key, :region, :bucket].each do |key|
+            raise <<-EOS.unindent unless attributes.key?(key)
+              #{key} is required.
+            EOS
+          end
         end
 
         def run
