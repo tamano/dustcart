f550a076a981af0b56c6884a6dece2e8ee26c60a
Yuya TAMANO <everfree.main@gmail.com>
2016-08-24 21:10:16 +0900

    Add comments to sample file

--- spec/sample_instructions/backup_all.rb
+++ spec/sample_instructions/backup_all.rb
@@ -1,21 +1,30 @@
+# where to put backup data temporary before uploading to S3
 dump_site '/tmp/backup'
 
+# which data to backup.
 group :input do
-  directory '/tmp/target_directory' do
-  end
+  # Most simple way to backup directory
+  directory '/tmp/target_directory'
 
+  # Label can change directory's name in backup
   directory '/tmp/target_directory2' do
     label 'backup1'
   end
 
-  directory '/tmp/target_directory3'
+  # This also works
+  directory '/tmp/target_directory3' do
+  end
 
+  # Backup files this way
   file '/tmp/target_file' do
     label 'backup_file'
   end
 end
 
+# where to upload
 group :output do
+  # Specifying Amazon S3 to backup all data in :input group
+  # This is the only option just for now
   amazon_s3 :all do
     access_key_id 'MY_ACCESS_KEY_ID'
     secret_access_key 'MY_SECRET_ACCESS_KEY'
@@ -24,4 +33,6 @@ group :output do
   end
 end
 
+# cleanup the dump_site.
+# Target can be :all or :except_latest
 cleanup :except_latest
